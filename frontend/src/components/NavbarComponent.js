35ac5577d4f7ecbe0993e749cb9ecf433478241e
“saiteja” <kolupurisaiteja333@gmail.com>
2024-04-21 19:33:26 -0400

    cart link navabr

--- frontend/src/components/NavbarComponent.js
+++ frontend/src/components/NavbarComponent.js
@@ -22,25 +22,11 @@ export default function NavbarComponent() {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            {/* <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown> */}
-          </Nav>
+          <Nav className="me-auto"></Nav>
           <Nav>
             {session?._id ? (
               <>
-                <Nav.Link>
+                <Nav.Link to="/cart" as={Link}>
                   <Cart />
                   {cart?.length > 0 && (
                     <Button className="btn-sm">{cart?.length}</Button>
